f19520ff3f7270eb99f2cc5cc9a0d1535cc6ae3e
William Jenkins <wfjenkins@ucsd.edu>
2024-08-06 00:13:11 -0700

    Rm unused

--- src/tritonoa/data/hydrophone.py
+++ src/tritonoa/data/hydrophone.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from dataclasses import dataclass
-from typing import Union
 
 
-@dataclass
-class Hydrophone:
-    """Hydrophone.
+# @dataclass
+# class Hydrophone:
+#     """Hydrophone.
 
-    Args:
-        serial_number (int, optional): Serial number. Defaults to 0.
-        fixed_gain (float, optional): Fixed gain [dB]. Defaults to 0.0.
-        sensitivity (float, optional): Sensitivity [dB]. Defaults to 0.0.
-    """
+#     Args:
+#         serial_number (int, optional): Serial number. Defaults to 0.
+#         fixed_gain (float, optional): Fixed gain [dB]. Defaults to 0.0.
+#         sensitivity (float, optional): Sensitivity [dB]. Defaults to 0.0.
+#     """
 
-    serial_number: int = 0
-    fixed_gain: float = 0.0
-    sensitivity: float = 0.0
+#     serial_number: int = 0
+#     fixed_gain: float = 0.0
+#     sensitivity: float = 0.0
 
 
 @dataclass
@@ -24,16 +23,9 @@ class HydrophoneSpecs:
     """Specifications for one or more hydrophones.
 
     Args:
-        fixed_gain (Optional[list[float]], optional): Fixed gain [dB]. Defaults to 0.0.
-        sensitivity (Optional[list[float]], optional): Sensitivity [dB]. Defaults to 0.0.
-        serial_number (Optional[list[int]], optional): Serial number. Defaults to 0.
+        gain (list[float] | optional): Gain [dB], converts counts to V. Defaults to 0.0 (no effect).
+        sensitivity (list[float] | optional): Sensitivity [dB], converts V to uPa. Defaults to 0.0 (no effect).
     """
 
-    fixed_gain: Union[float, list[float]] = 0.0
-    sensitivity: Union[float, list[float]] = 0.0
-    serial_number: Union[float, list[int]] = 0
-
-
-@dataclass
-class CalibrationData:
-    ...
+    gain: float | list[float] = 0.0
+    sensitivity: float | list[float] = 0.0
