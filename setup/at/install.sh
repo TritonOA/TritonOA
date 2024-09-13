f1582371949f798aca824915b6c58ad264e176ed
William Jenkins <wfjenkins@ucsd.edu>
2024-09-13 16:03:37 -0700

    Improve AT installation script; remove source files, consolidate bin files

--- setup/at/install.sh
+++ setup/at/install.sh
@@ -4,27 +4,45 @@
 
 URL="http://oalib.hlsresearch.com/AcousticsToolbox/at_2023_5_18.zip"
 DLDEST=src/tmp
-BINDIR=src/bin
+LIBDIR=src/lib
 SETUPDIR=setup/at
 
 if [ ! -d $DLDEST ]; then
     mkdir -p $DLDEST
 fi
-if [ ! -d $BINDIR ]; then
-    mkdir -p $BINDIR
+if [ ! -d $LIBDIR ]; then
+    mkdir -p $LIBDIR
 fi
 
 curl -o "$DLDEST/at.zip" $URL
-tar -xvf "$DLDEST/at.zip" -C $BINDIR
+tar -xvf "$DLDEST/at.zip" -C $LIBDIR
 rm "$DLDEST/at.zip"
 
 if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "arm64" ]; then
     echo "Using Makefile.mac-arm64"
-    cp $SETUPDIR/Makefile.mac-arm64 $BINDIR/at/Makefile
+    cp $SETUPDIR/Makefile.mac-arm64 $LIBDIR/at/Makefile
 else
     echo "Acoustics Toolbox is not supported on this architecture."
 fi
 
-cd $BINDIR/at
+cd $LIBDIR/at
 make clean
 make
+
+mkdir bin
+cp Bellhop/bellhop.exe bin/
+cp Bellhop/bellhop3d.exe bin/
+cp Kraken/bounce.exe bin/
+cp Kraken/kraken.exe bin/
+cp Kraken/krakenc.exe bin/
+cp KrakenField/field.exe bin/
+cp KrakenField/field3d.exe bin/
+cp Scooter/scooter.exe bin/
+cp Scooter/sparc.exe bin/
+
+find . -mindepth 1 \
+    -path './bin' -prune -o \
+    -path './doc' -prune -o \
+    -not -name 'index.html' \
+    -not -name 'Makefile' \
+    -exec rm -rf {} +
