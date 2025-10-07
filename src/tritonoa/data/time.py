Fix syntax of datetime precision.

src/tritonoa/data/time.py
@@ -278,5 +278,5 @@ def datetime_range(start: np.datetime64, end: np.datetime64, step: int) -> np.nd
     start_int = start.astype("int64")
     end_int = end.astype("int64")
     return np.arange(start_int, end_int, step, dtype="int64").astype(
-        f"datetime64({TIME_PRECISION})"
+        f"datetime64[{TIME_PRECISION}]"
     )